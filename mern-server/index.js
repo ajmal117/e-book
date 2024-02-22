7c26dd893375fad44d37bd9cd76ab8cae2189ce3
ajmal ali <ajmal.mzn95@gmail.com>
2024-02-22 21:00:11 +0530

    db link added

--- mern-server/index.js
+++ mern-server/index.js
@@ -21,7 +21,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // const uri = "mongodb://127.0.0.1:27017/BookInventory";
 
 const uri =
-  "mongodb+srv://user:user123@cluster0.lkshd4d.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://user:user123@cluster0.lkshd4d.mongodb.net/BookInventory?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {

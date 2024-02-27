b9078a9ea9da22f29cfd7de00d10a977ebd7d7ed
ajmal ali <ajmal.mzn95@gmail.com>
2024-02-27 20:32:14 +0530

    backend link updated

--- mern-client/src/home/BestSellerBooks.jsx
+++ mern-client/src/home/BestSellerBooks.jsx
@@ -5,8 +5,8 @@ function BestSellerBooks() {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    // fetch("https://e-book-pied.vercel.app/all-books")
-    fetch("http://localhost:8000/all-books")
+    fetch("https://e-book-pied.vercel.app/all-books")
+    // fetch("http://localhost:8000/all-books")
       .then((res) => res.json())
       .then((data) => setBooks(data.slice(0, 8)))
       .catch((err) => console.log(err));
